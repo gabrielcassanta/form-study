4b369d5291763ba3bcf5f746659eddfb0cc43977
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-08-01 22:28:55 -0300

    arrow func

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var form = document.querySelectorAll("#cadastro");
 var botaoAdicionar = document.querySelector("#enviar");
 
-botaoAdicionar.addEventListener("click", function (event) {
-  event.preventDefault();
+botaoAdicionar.addEventListener("click", (e) => {
+  e.preventDefault();
   console.log("fui clicado");
 
 })
