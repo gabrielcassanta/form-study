5ec66b5dfb1dda8b848bbcdbca6503f9d725077e
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-08-01 22:23:58 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var form = document.querySelectorAll("#cadastro");
+var botaoAdicionar = document.querySelector("#enviar");
+
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("fui clicado");
+
+})
